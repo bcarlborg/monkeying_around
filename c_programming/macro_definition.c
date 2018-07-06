2306e5cd92b41ba56c6ee607f351b74d6b604a95
Beau Carlborg <bcarlborg@gmail.com>
2018-07-05 22:40:06 -0600

    more monkeying with scanning and with macros

---
+++ c_programming/macro_definition.c
@@ -0,0 +1,9 @@
+#define MY_FIRST_MACRO 20
+
+#include <stdio.h>
+
+int main(void) {
+    int n = MY_FIRST_MACRO;
+    printf("beau's first macro constant had the value of %d\n", n);
+    return 0;
+}
---
+++ c_programming/scanning.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int entered_integer; 
+    printf("Please enter a number please: ");
+    scanf("%d", &entered_integer);
+    printf("your number was %d\n", entered_integer);
+    return 0;
+} 
