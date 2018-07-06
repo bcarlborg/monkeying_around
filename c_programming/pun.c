b88fc2053c270b88d8a732dd0b0eee79d361bf1d
Beau Carlborg <bcarlborg@gmail.com>
2018-07-05 22:25:42 -0600

    simple printing function

---
+++ c_programming/pun.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+    int a, b, c;
+    a = 10;
+    b = 10;
+    c = 10;
+    printf("%d", a);
+    printf("%d", b);
+    printf("%d", c);
+    printf("To C or not to C: That os the question\n");
+    return 0;
+}
